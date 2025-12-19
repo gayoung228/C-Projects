168ed5669e26cb0f579651787f44105732af708a
이가영 <103436798+gayoung228@users.noreply.github.com>
2025-12-19 17:34:32 +0900

    Update BookManageProgram_struct.c

--- 도서 관리 프로그램/BookManageProgram_struct.c
+++ 도서 관리 프로그램/BookManageProgram_struct.c
@@ -9,14 +9,14 @@ typedef struct {
     char author[30];    // 저자
     char publisher[30]; // 출판사
     int borrowed;       // 0: 대출 가능, 1: 대출 불가능
-} Book;
+} Book; 
 
 int add_book(Book books[], int* num_total_book);
 int search_book(Book books[], int num_total_book);
 int borrow_book(Book books[], int num_total_book);
 int return_book(Book books[], int num_total_book);
 int compare(char* str1, char* str2);
-int search_option();
+int search_option(); 
 void print_book_info(int id, Book books[]);
 const char* borrowed_status(int borrowed);
 
